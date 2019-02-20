51e74e1d88bb6a1e2ea9de577134efbd2a4773c1
Vlad Nicu <nicuvlad89@gmail.com>
2019-02-20 16:41:21 +0200

    created brewery and receipes interfaces

--- src/app/shared/services/receipes.service.ts
+++ src/app/shared/services/receipes.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+export interface IReceipe {
+  method: string;
+  style: string;
+  boil_time: string;
+  boil_size: string;
+  o_gravity: string;
+  f_gravity: string;
+  abv: string;
+  ibu: string;
+  srm: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,25 +20,25 @@ export class ReceipesService {
 
   // TODO improve the URLS later
   private _receipesUrl = 'http://homebrewing.test:8000/api/breweries/3/receipes';
-  // private _storeReceipeUrl = 'http://homebrewing.test:8000/api/breweries/';
-  // private _updateReceipeUrl = 'http://homebrewing.test:8000/api/breweries/6';
-  // private _deleteReceipeUrl = 'http://homebrewing.test:8000/api/breweries/6';
+  private _storeReceipeUrl = 'http://homebrewing.test:8000/api/breweries/';
+  private _updateReceipeUrl = 'http://homebrewing.test:8000/api/breweries/6';
+  private _deleteReceipeUrl = 'http://homebrewing.test:8000/api/breweries/6';
 
   constructor(private http: HttpClient) { }
 
-  getReceipes(){
+  getReceipes() {
     return this.http.get<any>(this._receipesUrl);
   }
 
-  // storeReceipe(receipe) {
-  //   return this.http.post<any>(this._storeReceipeUrl, JSON.stringify(receipe));
-  // }
+  storeReceipe(receipe) {
+    return this.http.post<any>(this._storeReceipeUrl, JSON.stringify(receipe));
+  }
 
-  // updateReceipe(receipe) {
-  //   return this.http.patch<any>(this._updateReceipeUrl, JSON.stringify(receipe));
-  // }
+  updateReceipe(receipe) {
+    return this.http.patch<any>(this._updateReceipeUrl, JSON.stringify(receipe));
+  }
 
-  // deleteReceipe() {
-  //   return this.http.delete<any>(this._deleteReceipeUrl);
-  // }
+  deleteReceipe() {
+    return this.http.delete<any>(this._deleteReceipeUrl);
+  }
 }
