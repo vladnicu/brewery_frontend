a9f42dfbabbd361c284e9643ae10eff1f54863e7
Vlad Nicu <nicuvlad89@gmail.com>
2019-02-20 13:23:05 +0200

    refactoring structure 2: authorized structure

---
+++ src/app/authorized/authorized.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthorizedRoutingModule } from './authorized-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthorizedRoutingModule
+  ]
+})
+export class AuthorizedModule { }
